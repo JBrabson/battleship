2bb6bd4e89c3df66e7c7e31d42aa72f5283806da
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2020-12-08 12:49:03 -0700

    Add length method and health method for ship.rb

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,10 +1,19 @@
 class Ship
-  def initialize(name, argument2)
+  def initialize(name, length)
     @name = name
+    @length = length
   end
 
   def name
     @name
   end
-  
+
+  def length
+    @length
+  end
+
+  def health
+    @length
+  end
+
 end
