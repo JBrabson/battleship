1e439300dd240568390f6a66fa40442f5bdec418
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2020-12-10 16:02:59 -0700

    Add test_it_has_cells in board_test.rb

--- test/board_test.rb
+++ test/board_test.rb
@@ -5,10 +5,34 @@ require "./lib/cell"
 require "./lib/board"
 
 class BoardTest < Minitest::Test
-  
+
   def test_it_exists
     board = Board.new
 
     assert_instance_of Board, board
+  end
+
+  def test_it_has_cells
+    board = Board.new
+    assert_equal 16, board.cells.length
+    assert_instance_of Cell, board.cells["A1"]
+    assert_instance_of Cell, board.cells["A2"]
+    assert_instance_of Cell, board.cells["A3"]
+    assert_instance_of Cell, board.cells["A4"]
+    assert_instance_of Cell, board.cells["B1"]
+    assert_instance_of Cell, board.cells["B2"]
+    assert_instance_of Cell, board.cells["B3"]
+    assert_instance_of Cell, board.cells["B4"]
+    assert_instance_of Cell, board.cells["C1"]
+    assert_instance_of Cell, board.cells["C2"]
+    assert_instance_of Cell, board.cells["C3"]
+    assert_instance_of Cell, board.cells["C4"]
+    assert_instance_of Cell, board.cells["D1"]
+    assert_instance_of Cell, board.cells["D2"]
+    assert_instance_of Cell, board.cells["D3"]
+    assert_instance_of Cell, board.cells["D4"]
+
+
+
   end
 end
