a3875e54381158e0d8045b852bdce4ca72ef055d
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2020-12-16 16:27:41 -0700

    Add test_it_has_attributes; passing.

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,5 +1,9 @@
 class Computer
-	attr_reader :computer_board
+	attr_reader :computer_board,
+							:ship_1,
+							:ship_2,
+							:possible_three_place,
+							:possible_two_place
 
 	def initialize(ship_1, ship_2)
 		@ship_1 = ship_1
@@ -9,4 +13,8 @@ class Computer
 		@possible_two_place = [["B1", "C1"], ["B4", "C4"], ["B2", "C2"]]
 	end
 
+	def position_enemy_ship_1(ship_1, coordinate_ary)
+		coordinate_ary = @possible_three_place.sample
+		computer_board.place(ship_1, coordinate_ary)
+	end
 end
--- test/computer_test.rb
+++ test/computer_test.rb
@@ -15,5 +15,22 @@ class ComputerTest < Minitest::Test
     assert_instance_of Computer, computer
   end
 
+	def test_it_has_attributes
+		cruiser = Ship.new("Cruiser", 3)
+		submarine = Ship.new("Submarine", 2)
+		computer = Computer.new(cruiser, submarine)
+		assert_equal cruiser, computer.ship_1
+		assert_equal submarine, computer.ship_2
+		assert_instance_of Board, computer.computer_board
+		assert_equal [["A1, A2", "A3"], ["B3", "C3", "D3"], ["D2", "D3", "D4"]], computer.possible_three_place
+		assert_equal [["B1", "C1"], ["B4", "C4"], ["B2", "C2"]], computer.possible_two_place
+	end
 
+	# def test_places_enemy_ship_1
+	# 	cruiser = Ship.new("Cruiser", 3)
+	# 	submarine = Ship.new("Submarine", 2)
+	# 	computer = Computer.new(cruiser, submarine)
+	# 	possible_coordinates = [["A1, A2", "A3"], ["B3", "C3", "D3"], ["D2", "D3", "D4"]]
+	# 	assert_equal true, possible_coordinates.include?(computer.position_enemy_ship_1(cruiser, possible_coordinates))
+	# end
 end
