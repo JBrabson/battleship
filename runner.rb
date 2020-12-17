9916e83cf004a8f1407a781712d9d8cacd9b7fc9
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-12-16 18:32:58 -0600

    Add code to place ships on player and computer boards accordingly.

--- runner.rb
+++ runner.rb
@@ -20,3 +20,17 @@ if choice == "p"
   computer.position_enemy_ship_1(enemy_cruiser)
   computer.position_enemy_ship_2(enemy_submarine)
   p "I have laid out my ships on the grind."
+
+  # Placing Ships
+  p "You now need to lay out your two ship."
+p "The #{player_cruiser.name} is three units long and the #{player_submarine.name} is two units long."
+puts player.player_board.render(false)
+p "Enter the squares for the #{player_cruiser.name}"
+p "Enter first square"
+cruiser_coor1 = gets.chomp.upcase
+p "Enter second square"
+cruiser_coor2 = gets.chomp.upcase
+p "Enter third square"
+cruiser_coor3 = gets.chomp.upcase
+cruiser_coordinates = ["#{cruiser_coor1}", "#{cruiser_coor2}", "#{cruiser_coor3}"]
+p "Invalid Coordinates --ERROR--" if (player.player_board.valid_placement?(player_cruiser, cruiser_coordinates) == false)
